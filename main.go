88b22ba6dd68de9ec860754c21c1d7db8ba16d51
caosheng <caosheng@bytedance.com>
2016-06-07 12:37:29 +0800

    add base64 echo
    Change-Id: Ida08285e6437eda3bf88771067db1482db77dcff

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -11,7 +12,13 @@ func echo(w http.ResponseWriter, r *http.Request) {
 	strText := string(text)
 	fmt.Fprint(w, strText)
 }
+func echoBase64(w http.ResponseWriter, r *http.Request) {
+	text, _ := ioutil.ReadAll(r.Body)
+	strText := base64.StdEncoding.EncodeToString(text)
+	fmt.Fprint(w, strText)
+}
 func main() {
 	http.HandleFunc("/echo", echo)
+	http.HandleFunc("/echo_base64", echoBase64)
 	http.ListenAndServe(":8192", nil)
 }
